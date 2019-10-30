57d8952e4b7237fc86bce442757c79bcf70b45c1
koyeo <im@koyeo.io>
2019-10-30 17:56:29 +0800

    add pipe

--- enums/flags.go
+++ enums/flags.go
@@ -1,7 +1,7 @@
 package enums
 
 const (
-	DeployFlag       = "deploy,-d"
+	DeployFlag       = "deploy,d"
 	DeployUsage      = "Deploy after build"
 	PrintScriptFlag  = "print_script"
 	PrintScriptUsage = "Print script on console"
