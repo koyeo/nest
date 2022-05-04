ad2455d5e911b287ae449c1cf270ebb3a6cacb5c
koyeo <koeyo@qq.com>
2022-05-04 22:52:58 +0800

    fix tml

--- cmd/initialize/cmd.go
+++ cmd/initialize/cmd.go
@@ -74,7 +74,7 @@ tasks:
       # 引用其它任务的全部 steps
       - use: task-1
       # 在本地执行命令
-      - run: echo second            
+      - run: echo 'Hi! this is from second task!'            
       # 部署服务器
       - deploy:
           servers:
@@ -82,9 +82,9 @@ tasks:
             - use: server-1                    
           mappers:                             
             # 本地文件路径
-            - source: ./foo                    
-			  # 服务器存放位置		
-              target: /app/foo/bin/foo         
+            - source: ./hi.txt                    
+            # 服务器存放位置		
+              target: /app/foo/hi         
           executes:
             # 在服务器执行命令
             - run: echo 'Hi! this is from server-1'
